e8e7e71efec3c18822c37cbf7530ebdc0d166bb4
Sam Zofkie <samzofkie@gmail.com>
2022-12-12 12:50:43 -0500

    Added downloading xcompiler tarballs

--- build_cross_compiler.py
+++ build_cross_compiler.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 import os
+import urllib.request
 
-lfs = "./lfs"
+lfs = "/home/lfs/lfs"
 
 lfs_dir_structure = [ "etc", "var", "usr", "tools",
                       "lib64",
@@ -17,9 +18,17 @@ def create_dir_structure():
     except FileExistsError:
         pass
 
+tarball_urls = { "binutils" : "https://ftp.gnu.org/gnu/binutils/binutils-2.39.tar.xz",
+                 "gcc" : "https://ftp.gnu.org/gnu/gcc/gcc-12.2.0/gcc-12.2.0.tar.xz",
+                 "linux" : "https://www.kernel.org/pub/linux/kernel/v5.x/linux-5.19.2.tar.xz",
+                 "glibc" : "https://ftp.gnu.org/gnu/glibc/glibc-2.36.tar.xz" }
 
-os.environ["LFS"]="./lfs"
+
+os.environ["LFS"]="/home/lfs/lfs"
 create_dir_structure()
-print(os.getcwd())
+os.chdir(lfs)
 
-
+for name in tarball_urls:
+    print("downloading " + tarball_urls[name])
+    tarball_file_name = "/home/lfs/lfs/" + tarball_urls[name].split("/")[-1]
+    urllib.request.urlretrieve(tarball_urls[name], tarball_file_name )
