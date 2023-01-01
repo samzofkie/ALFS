07e6c1126a36b989843aee6d03fc31f1ac061be3
Sam Zofkie <samzofkie@gmail.com>
2023-01-01 14:12:32 -0500

    Reworked Dockerfile to automatically execute python script

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,16 @@ FROM alpine:3.17
 RUN apk add --no-cache bash binutils bison coreutils \
 	diffutils findutils gawk gcc grep gzip m4 make \
 	patch perl python3 shadow sed tar texinfo xz
+
+# Create lfs user and group
 RUN groupadd lfs
 RUN useradd -s /bin/bash -g lfs -m -k /dev/null lfs
 USER lfs
+
+# Switch to working dir
+WORKDIR /home/lfs
+
+# Copy Python script
+COPY --chown=lfs:lfs ./build_cross_compiler.py .
+RUN ./build_cross_compiler.py
+
--- build_cross_compiler.py
+++ build_cross_compiler.py
@@ -3,7 +3,6 @@
 import os
 import urllib.request
 
-lfs = "/home/lfs/lfs"
 
 lfs_dir_structure = [ "etc", "var", "usr", "tools",
                       "lib64",
@@ -11,24 +10,30 @@ lfs_dir_structure = [ "etc", "var", "usr", "tools",
 
 def create_dir_structure():
     try: 
-        os.mkdir(lfs)
-        os.chdir(lfs)
+        os.mkdir(os.environ["LFS"])
+        os.chdir(os.environ["LFS"])
         for directory in lfs_dir_structure:
             os.mkdir(directory)
     except FileExistsError:
         pass
+    print("created directory structure in " + os.environ["LFS"])
 
 tarball_urls = { "binutils" : "https://ftp.gnu.org/gnu/binutils/binutils-2.39.tar.xz",
                  "gcc" : "https://ftp.gnu.org/gnu/gcc/gcc-12.2.0/gcc-12.2.0.tar.xz",
                  "linux" : "https://www.kernel.org/pub/linux/kernel/v5.x/linux-5.19.2.tar.xz",
                  "glibc" : "https://ftp.gnu.org/gnu/glibc/glibc-2.36.tar.xz" }
 
+def download_and_unpack_sources():
+    for name in tarball_urls:
+        print("downloading " + tarball_urls[name] + "...")
+        tarball_filename = os.environ["LFS"] + "/" + tarball_urls[name].split("/")[-1]
+        urllib.request.urlretrieve(tarball_urls[name], tarball_filename)
+        print("unpacking " + tarball_filename + "...")
+        os.system("tar -xvf " + tarball_filename + " > /dev/null")
+        print("removing " + tarball_filename)
+        os.system("rm " + tarball_filename)
 
-os.environ["LFS"]="/home/lfs/lfs"
+os.environ["LFS"] = "/home/lfs/lfs"
 create_dir_structure()
-os.chdir(lfs)
+download_and_unpack_sources()
 
-for name in tarball_urls:
-    print("downloading " + tarball_urls[name])
-    tarball_file_name = "/home/lfs/lfs/" + tarball_urls[name].split("/")[-1]
-    urllib.request.urlretrieve(tarball_urls[name], tarball_file_name )
