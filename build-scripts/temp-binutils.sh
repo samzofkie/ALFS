0e3fccfe0db50049232ef504f3132ba5c265dd9c
Sam Zofkie <samzofkie@gmail.com>
2023-01-20 09:53:27 -0500

    Added build scripts for temp-binutils and temp-gcc

---
+++ build-scripts/temp-binutils.sh
@@ -0,0 +1,16 @@
+set -e
+sed '6009s/$add_dir//' -i ltmain.sh
+mkdir -v build
+cd build
+../configure \
+--prefix=/usr \
+--build=$(../config.guess) \
+--host=$LFS_TGT \
+--disable-nls \
+--enable-shared \
+--enable-gprofng=no \
+--disable-werror \
+--enable-64-bit-bfd
+make
+make DESTDIR=$LFS
+rm -v $LFS/usr/lib/lib{bfd,ctf,ctf-nobfd,opcodes}.{a,la}
---
+++ build-scripts/temp-gcc.sh
@@ -0,0 +1,32 @@
+set -e
+sed -e '/m64=/s/lib64/lib/' -i.orig gcc/config/i386/t-linux64
+sed '/thread_header =/s/@.*@/gthr-posix.h/' \
+  -i libgcc/Makefile.in libstdc++-v3/include/Makefile.in
+
+SRC_DIR=$(pwd)
+mkdir -v ../gcc-build
+cd ../gcc-build
+
+$SRC_DIR/configure --build=$(../config.guess) \
+--host=$LFS_TGT \
+--target=$LFS_TGT \
+LDFLAGS_FOR_TARGET=-L$PWD/$LFS_TGT/libgcc \
+--prefix=/usr \
+--with-build-sysroot=$LFS \
+--enable-initfini-array \
+--disable-nls \
+--disable-multilib \
+--disable-decimal-float \
+--disable-libatomic \
+--disable-libgomp \
+--disable-libquadmath \
+--disable-libssp \
+--disable-libvtv \
+--enable-languages=c,c++
+make
+make DESTDIR=$LFS install
+ln -sv gcc $LFS/usr/bin/cc
+
+cd $LFS/srcs
+rm -rf gcc-build
+
