15c91fd1a2015ebaaedf3cff191d72ebf390c3fa
Sam Zofkie <samzofkie@gmail.com>
2023-02-02 19:00:29 -0500

    No make check for now. Still don't understand why it hangs subprocess.run unlike everything else

--- build-scripts/glibc.sh
+++ build-scripts/glibc.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 set -v
 patch -Np1 -i ../glibc-2.36-fhs-1.patch
@@ -12,10 +12,6 @@ echo "rootsbindir=/usr/sbin" > configparms
   --with-headers=/usr/include \
   libc_cv_slibdir=/usr/lib
 make
-set +e
-make check
-set -e
 touch /etc/ld.so.conf
 sed '/test-installation/s@$(PERL)@echo not running@' -i ../Makefile
 make install
-sed '/RTLDLIST=/s@/usr@@g' -i /usr/bin/ldd
