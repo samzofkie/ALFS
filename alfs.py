01441574ca2f0a57871af7ba4f2235233ba89aea
Sam Zofkie <samzofkie@gmail.com>
2023-07-08 15:00:10 -0500

    build function.

--- alfs.py
+++ alfs.py
@@ -7,19 +7,25 @@ import shutil
 
 
 class Alfs:
+    lfs_tgt = "x86_64-lfs-linux-gnu"
+    system_dirs = ["etc", "var", "usr/bin", "usr/lib", "usr/sbin", 
+                   "lib64", "tools"]
+
     def __init__(self):
         self.root_dir = os.getcwd()
-        self.lfs_tgt = "x86_64-lfs-linux-gnu"
         self.env_vars = { 
-             "LC_ALL": "POSIX", 
-             "PATH": f"{self.root_dir}/tools/bin:/usr/bin",
-             "CONFIG_SITE": f"{self.root_dir}/usr/share/config.site" 
+            "LC_ALL": "POSIX", 
+            "PATH": f"{self.root_dir}/tools/bin:/usr/bin",
+            "CONFIG_SITE": f"{self.root_dir}/usr/share/config.site" 
         }
-        self.system_dirs = ["etc", "var", "usr/bin", "usr/lib", "usr/sbin", 
-                            "lib64", "tools"]
+        self.ensure_wget_list()
+        self.ensure_directory_skeleton()
+        self.ensure_tarballs_downloaded()
+        self.all_files = self.system_snapshot()
 
 
     def ensure_wget_list(self):
+        os.chdir(self.root_dir)
         if not os.path.exists("wget-list"):
             res = urlopen("https://www.linuxfromscratch.org/lfs/" \
                 "downloads/stable/wget-list")
@@ -28,14 +34,30 @@ class Alfs:
 
 
     def ensure_directory_skeleton(self):
+        os.chdir(self.root_dir)
         for dir in self.system_dirs:
             if not os.path.exists(dir):
                 os.makedirs(dir)
         if not os.path.exists("sources"):
             os.mkdir("sources")
-    
+
+
+    def ensure_tarballs_downloaded(self):
+        os.chdir(self.root_dir)
+        with open("wget-list", "r") as f:
+            tarball_urls = [line.split("\n")[0] for line in f.readlines()]
+
+        for url in tarball_urls:
+            tarball_name = url.split("/")[-1]
+            if not os.path.exists("sources/" + tarball_name): 
+                print("downloading " + tarball_name + "...")
+                res = urlopen(url)
+                with open("sources/" + tarball_name, "wb") as f:
+                    f.write(res.read())
+
 
     def system_snapshot(self):
+        os.chdir(self.root_dir)
         dirs = self.system_dirs.copy()
         all_files = set()
         while dirs:
@@ -49,32 +71,8 @@ class Alfs:
         return all_files
 
 
-    def ensure_tarballs_downloaded(self):
-        with open("wget-list", "r") as f:
-            tarball_urls = [line.split("\n")[0] for line in f.readlines()]
-
-        for url in tarball_urls:
-            tarball_name = url.split("/")[-1]
-            if not os.path.exists("sources/" + tarball_name): 
-                print("downloading " + tarball_name + "...")
-                res = urlopen(url)
-                with open("sources/" + tarball_name, "wb") as f:
-                    f.write(res.read())
-
-
-    def build_system(self):
-        self.ensure_wget_list()
-        self.ensure_directory_skeleton()
-        self.ensure_tarballs_downloaded()
-        self.all_files = self.system_snapshot()
-        self.build_cross_binutils()
-        self.build_cross_gcc()
-        self.build_linux_headers()
-
-
     class TarballNotFoundError(Exception):
         pass
-
 
     def get_tarball_name(self, package_name):
         tarballs = os.listdir(f"{self.root_dir}/sources")
@@ -90,6 +88,7 @@ class Alfs:
 
 
     def untar_and_enter_source_dir(self, package_name):
+        os.chdir(self.root_dir)
         tarball_name = self.get_tarball_name(package_name)
         package_dir_name = tarball_name.split(".tar")[0] 
         if not os.path.exists(package_dir_name):
@@ -129,16 +128,36 @@ class Alfs:
         self.all_files = self.all_files.union(new_files)
 
 
+    def build(self,
+              search_term, 
+              configure_flags, 
+              tracked_file_path,
+              before_build=None,
+              after_build=None,
+              custom_build=None,
+              use_build_dir=True):
+        self.untar_and_enter_source_dir(search_term)
+        if before_build:
+            before_build()
+        if use_build_dir:
+            self.create_and_enter_build_dir()
+        if custom_build:
+            custom_build()
+        else:
+            self.run_build_commands(configure_flags)
+        if after_build:
+            after_build()
+        self.clean_up_build(search_term)
+        self.record_new_files(tracked_file_path)
+
+
     def build_cross_binutils(self):
-        self.untar_and_enter_source_dir("binutils")
-        self.create_and_enter_build_dir()
-        self.run_build_commands(f"--prefix={self.root_dir}/tools " \
+        configure_flags = f"--prefix={self.root_dir}/tools " \
             f"--with-sysroot={self.root_dir} --target={self.lfs_tgt} " \
-            "--disable-nls --enable-gprofng=no --disable-werror")
-        self.clean_up_build("binutils")
-        self.record_new_files("cross-binutils")
+            "--disable-nls --enable-gprofng=no --disable-werror"
+        self.build("binutils", configure_flags, "cross-binutils") 
 
-
+    
     def cross_gcc_before_build(self):
         for dep in ["mpfr", "gmp", "mpc"]:
             tarball_name = self.get_tarball_name(dep)
@@ -172,24 +191,22 @@ class Alfs:
 
 
     def build_cross_gcc(self):
-        self.untar_and_enter_source_dir("gcc")
-        self.cross_gcc_before_build()
-        self.create_and_enter_build_dir()
-        self.run_build_commands(f"--target={self.lfs_tgt} " \
+        configure_flags = f"--target={self.lfs_tgt} " \
             f"--prefix={self.root_dir}/tools --with-glibc-version=2.37 " \
             f"--with-sysroot={self.root_dir} --with-newlib " \
             "--without-headers --enable-default-pie --enable-default-ssp " \
             "--disable-nls --disable-shared --disable-multilib " \
             "--disable-threads --disable-libatomic --disable-libgomp " \
             "--disable-libquadmath --disable-libssp --disable-libvtv " \
-            "--disable-libstdcxx --enable-languages=c,c++")
-        self.cross_gcc_after_build()
-        self.clean_up_build("gcc")
-        self.record_new_files("cross-gcc")
-    
+            "--disable-libstdcxx --enable-languages=c,c++"
+        self.build("gcc", configure_flags, "cross-gcc",
+                   before_build=self.cross_gcc_before_build,
+                   after_build=self.cross_gcc_after_build)
 
-    @staticmethod
-    def copy_all_headers(source, destination):
+
+    def copy_linux_headers(self):
+        source = "usr/include"
+        destination = self.root_dir
         if not os.path.exists(destination):
             os.mkdir(destination)
         files = [source]
@@ -202,13 +219,26 @@ class Alfs:
                 shutil.copy(curr, destination + "/" + curr)
 
 
-    def build_linux_headers(self):
-        self.untar_and_enter_source_dir("linux-6")
+    def linux_headers_custom_build(self):
         self.run("make mrproper")
         self.run("make headers")
-        self.copy_all_headers("usr/include", self.root_dir)
-        self.clean_up_build("linux-6")
-        self.record_new_files("linux-headers")
+
+
+    def build_linux_headers(self):
+        self.build("linux-6", "", "linux-headers",
+                   after_build=self.copy_linux_headers,
+                   custom_build=self.linux_headers_custom_build,
+                   use_build_dir=False)
+
+
+    def build_system(self):
+        self.build_cross_binutils()
+        self.build_cross_gcc()
+        self.build_linux_headers()
+
+
+
+
         
 
 # see if i can get away with no pre and post modifications gcc 
