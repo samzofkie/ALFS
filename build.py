cf7eb8fdc7ac29d2f6294621ad1b8c9cd6640b07
Sam Zofkie <samzofkie@gmail.com>
2023-07-10 14:33:20 -0500

    Refactored into modules.

---
+++ build.py
@@ -0,0 +1,116 @@
+"""This module exports the build function which untars, configures, compiles and
+installs a target from its source code.
+
+Some vocab:
+    * 'target' is the name a specific build of a program from it's source code.
+      I'm using it to distinguish different programs that are built from the
+      same source code at different phases in the whole system build, for
+      example 'cross-binutils' refers to the build of the binutils utilities for
+      the cross compiler toolchain, at the beginning of the whole system build,
+      whereas plain 'binutils' is the target name for the build of the binutils
+      utilities for the final system.
+    * 'search term' is a name that can be given to _search_for_tarball that will
+      produce one source code tarball. It should be able to be derived from
+      'target', like 'cross-binutils' and 'binutils' should both map to
+      'binutils'.
+    * 'package name' refers to the search term, but with a version number.
+      'binutils' should become the string 'binutils-X.Y'. Note that this is
+      usually the same as the name of the directory holding all the untar-ed
+      source code.
+    * 'tarball name' refers to the package name with '.tar.xz' or '.tar.gz'
+      appended to it, depending on the format in wget-list / the source
+      directory.
+"""
+
+import os
+import subprocess
+import shutil
+from setup import ROOT_DIR, ENV_VARS, record_new_files
+
+
+def _clean_target_name(target_name):
+    search_term = target_name.replace("cross-","")
+    search_term = search_term.replace("temp-", "")
+    return search_term
+
+
+def _search_for_tarball(search_term):
+    tarballs = os.listdir(f"{ROOT_DIR}/sources")
+    res = [tarball for tarball in tarballs if search_term in tarball]
+    res = [tarball for tarball in res if ".patch" not in tarball]
+    if len(res) < 1:
+        raise FileNotFoundError("No results from search term \"" +
+                                   search_term + "\"")
+    elif len(res) > 1:
+        raise FileNotFoundError("Too many results from search term \"" +
+                                search_term + "\": " + str(res))
+    return res[0]
+
+
+def get_tarball_and_package_names(search_term):
+    tarball_name = _search_for_tarball(search_term)
+    package_name = tarball_name.split(".tar")[0]
+    return tarball_name, package_name
+
+
+def _untar_and_enter_source_dir(tarball_name, package_name):
+    os.chdir(ROOT_DIR)
+    if not os.path.exists(package_name):
+        subprocess.run(("tar -xvf sources/" + tarball_name).split(" "))
+    os.chdir(package_name)
+
+
+def create_and_enter_build_dir():
+    if not os.path.exists("build"):
+        os.mkdir("build")
+    os.chdir("build")
+
+
+def run(command):
+    subprocess.run(command.split(), env=ENV_VARS, check=True)
+
+
+def _run_build_commands(configure_command, destdir):
+    install_command = "make install"
+    if destdir:
+        install_command.replace(" ", f" DESTDIR={destdir} ")
+    build_commands = [configure_command, "make", install_command]
+    for command in build_commands:
+        run(command)
+
+
+def _clean_up_build(package_name):
+    os.chdir(ROOT_DIR)
+    shutil.rmtree(package_name)
+
+
+def build(target_name,
+          search_term = "",
+          before_build = None,
+          build_dir = True,
+          configure_command = "",
+          destdir = "",
+          after_build = None):
+    """Compile and install a target from source code.
+
+    """
+    os.chdir(ROOT_DIR)
+    if not search_term:
+        search_term = _clean_target_name(target_name)
+    tarball_name, package_name = get_tarball_and_package_names(search_term)
+    
+    _untar_and_enter_source_dir(tarball_name, package_name)
+    if before_build: 
+        before_build()
+    if build_dir:
+        create_and_enter_build_dir()
+    if configure_command:
+        _run_build_commands(configure_command, destdir)
+    if after_build:
+        after_build()
+    _clean_up_build(package_name)
+
+    record_new_files(target_name)
+
+    
+
---
+++ cross_toolchain.py
@@ -0,0 +1,137 @@
+import shutil
+import os
+import subprocess
+from setup import ROOT_DIR, LFS_TGT, ENV_VARS, HOST_TRIPLET
+from build import build, get_tarball_and_package_names, run
+
+
+def build_cross_binutils():
+    cc = (f"../configure --prefix={ROOT_DIR}/tools "
+        f"--with-sysroot={ROOT_DIR} --target={LFS_TGT} "
+        "--disable-nls --enable-gprofng=no --disable-werror")
+    build("cross-binutils", configure_command = cc)
+
+
+def cross_gcc_before():
+    for dep in ["mpfr", "gmp", "mpc"]:
+        tarball_name, package_name = get_tarball_and_package_names(dep)
+        run(f"tar -xvf {ROOT_DIR}/sources/{tarball_name}")
+        run(f"mv {package_name} {dep}")
+
+    shutil.copyfile("gcc/config/i386/t-linux64", 
+                    "gcc/config/i386/t-linux64.orig")
+    with open("gcc/config/i386/t-linux64", "r") as f:
+        lines = f.readlines()
+    for line in lines:
+        if "m64=" in line:
+            line = line.replace("lib64", "lib")
+    with open("gcc/config/i386/t-linux64", "w") as f:
+        f.writelines(lines)
+
+
+def cross_gcc_after():
+    os.chdir("..")
+    lines = []
+    for file in ["gcc/limitx.h", "gcc/glimits.h", "gcc/limity.h"]:
+        with open(file, "r") as f:
+            lines += f.readlines()
+    completed = subprocess.run(
+        f"{LFS_TGT}-gcc -print-libgcc-file-name".split(" "),
+        capture_output=True, env=ENV_VARS)
+    dirname = completed.stdout.decode().rsplit("/",1)[0]
+    with open (dirname + "/install-tools/include/limits.h", "w") as f:
+        f.writelines(lines)
+
+
+def build_cross_gcc():
+    cc = (f"../configure --target={LFS_TGT} --prefix={ROOT_DIR}/tools "
+        f"--with-glibc-version=2.37 --with-sysroot={ROOT_DIR} --with-newlib "
+        "--without-headers --enable-default-pie --enable-default-ssp "
+        "--disable-nls --disable-shared --disable-multilib --disable-threads "
+        "--disable-libatomic --disable-libgomp --disable-libquadmath "
+        "--disable-libssp --disable-libvtv --disable-libstdcxx "
+        "--enable-languages=c,c++")
+    build("cross-gcc", before_build = cross_gcc_before, configure_command = cc, 
+          after_build = cross_gcc_after)
+
+
+def copy_linux_headers():
+    destination = ROOT_DIR
+    files = ["usr/include"]
+    while files:
+        curr = files.pop()
+        if os.path.isdir(curr):
+            files += [curr + "/" + f for f in os.listdir(curr)]
+            os.mkdir(destination + "/" + curr)
+        elif curr[-2:] == ".h":
+            shutil.copy(curr, destination + "/" + curr)
+
+
+def linux_headers_before():
+    run("make mrproper")
+    run("make headers")
+    copy_linux_headers()
+
+
+def build_linux_headers():
+    build("linux-headers", search_term = "linux-6", 
+          before_build = linux_headers_before, build_dir = False)
+
+
+def cross_glibc_before():
+    os.symlink("../usr/lib/ld-linux-x86-64.so.2", ROOT_DIR +
+               "/lib64/ld-linux-x86-64.so.2")
+    os.symlink("../usr/lib/ld-linux-x86-64.so.2", ROOT_DIR + 
+               "/lib64/ld-lsb-x86-64.so.3")
+    _, package_name = get_tarball_and_package_names("glibc")
+    run(f"patch -Np1 -i {ROOT_DIR}/sources/{package_name}-fhs-1.patch")
+    
+    if not os.path.exists("build"):
+        os.mkdir("build")
+    with open("build/configparms", "w") as f:
+        f.write("rootsbindir=/usr/sbin")
+
+
+def cross_glibc_after():
+    with open("main.c", "w") as f:
+        f.write("int main(){}")
+    run(f"{LFS_TGT}-gcc -xc main.c")
+    completed_process = subprocess.run("readelf -l a.out".split(" "),
+                                       env=ENV_VARS, check=True,
+                                       capture_output=True)
+    assert ( "[Requesting program interpreter: /lib64/ld-linux-x86-64.so.2]" 
+            in completed_process.stdout.decode() )
+    run(f"{ROOT_DIR}/tools/libexec/gcc/{LFS_TGT}/12.2.0/install-tools/mkheaders") 
+
+
+def build_cross_glibc():
+    cc = (f"../configure --prefix=/usr --host={LFS_TGT} --build={HOST_TRIPLET} "
+        f"--enable-kernel=3.2 --with-headers={ROOT_DIR}/usr/include "
+        "libc_cv_slibdir=/usr/lib")
+    build("cross-glibc", before_build = cross_glibc_before, 
+          configure_command = cc, destdir = ROOT_DIR, 
+          after_build = cross_glibc_after)
+
+
+def cross_libstdcpp_after():
+    for prefix in ["stdc++", "stdc++fs", "supc++"]:
+        archive = self.root_dir + "/usr/lib/lib" + prefix + ".la"
+        if os.path.exists(archive):
+            os.remove(archive)
+
+
+def build_cross_libstdcpp():
+    cc = (f"../libstdc++-v3/configure --host={LFS_TGT} --build={HOST_TRIPLET} "
+        "--prefix=/usr --disable-multilib --disable-nls "
+        "--disable-libstdcxx-pch "
+        f"--with-gxx-include-dir=/tools/{LFS_TGT}/include/c++/12.2.0")
+    build("cross-libstdc++", search_term = "gcc", configure_command = cc,
+          destdir = ROOT_DIR, after_build = cross_libstdcpp_after()) 
+
+
+def build_cross_toolchain():
+    build_cross_binutils()
+    build_cross_gcc()
+    build_linux_headers()
+    build_cross_glibc()
+    build_cross_libstdcpp()
