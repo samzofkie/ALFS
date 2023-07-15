c9f04ba5275e23bf9ad0da273da82d37fe85f7d1
Sam Zofkie <samzofkie@gmail.com>
2023-07-15 13:38:51 -0500

    Wrote _make_additional_dirs function, and added etc files to repo.

--- alfs.py
+++ alfs.py
@@ -2,19 +2,13 @@
 
 from cross_toolchain import build_cross_toolchain
 from temp_tools import build_temp_tools
-from chroot import mount_and_enter_chroot
+from chroot import prepare_and_enter_chroot
 
 
 if __name__ == '__main__':
     build_cross_toolchain()
     build_temp_tools()
-    mount_and_enter_chroot()
+    prepare_and_enter_chroot()
 
 # TODO
 # * tracked file checking / remove package function
- 
-
-
-        
-
-
--- chroot.py
+++ chroot.py
@@ -1,8 +1,8 @@
-import os
+import os, subprocess, shutil, stat
 from setup import ROOT_DIR
 
 
-def mount_vkfs(): 
+def _mount_vkfs(): 
     """ Mount Virtual Kernel Filesystems"""
     for dir in ["dev", "proc", "sys", "run"]:
         os.mkdir(f"{ROOT_DIR}/{dir}")
@@ -15,7 +15,7 @@ def mount_vkfs():
             exit(f"Command: '{command}' failed")
 
 
-def enter_chroot():
+def _enter_chroot():
     os.chdir(ROOT_DIR)
     os.chroot(ROOT_DIR)
     for var in os.environ:
@@ -23,6 +23,40 @@ def enter_chroot():
     os.environ["PATH"] = "/usr/bin:/usr/sbin" 
 
 
-def mount_and_enter_chroot():
-    mount_vkfs()
-    enter_chroot()
+def _make_additional_dirs():
+    dirs = ["boot", "home/tester", "mnt", "opt", "srv", "root", 
+            "usr/lib/firmware"]
+    dirs += ["etc/" + d for d in ["opt", "sysconfig"]]
+    dirs += ["media/" + d for d in ["floppy", "cdrom"]]
+    dirs += ["usr/local/" + d for d in ["bin", "lib", "sbin"]]
+    for prefix in ["usr/", "usr/local/"]:
+        dirs += [prefix + d for d in ["include", "src"]]
+        dirs += [prefix + "share/" + d for d in ["color", "dict", "doc", 
+                                                 "info", "locale", "man",
+                                                 "misc", "terminfo", 
+                                                 "zoneinfo"]]
+        dirs += [prefix + "share/man/man" + str(i) for i in range(9)]
+    dirs += ["var/" + d for d in ["cache", "local", "log", "mail", "opt",
+                                  "spool", "tmp"]]
+    dirs += ["var/lib/" + d for d in ["color", "misc", "locate"]]
+    for d in dirs:
+        os.makedirs(d)
+    os.symlink("/run", "/var/run")
+    os.symlink("/run/lock", "/var/lock")
+    subprocess.run("chmod 1777 /tmp /var/tmp", check=True)
+    shutil.chown("/home/tester", user="tester")
+    for file in ["btmp", "lastlog", "faillog", "wtmp"]:
+        with open("/var/log/" + file, "a") as f:
+            pass
+    shutil.chown("/var/log/lastlog", group="utmp")
+    os.chmod("/var/log/lastlog", stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP |
+             stat.S_IWGRP | stat.S_IROTH)
+    os.chmod("/var/log/btmp", stat.S_IRUSR | stat.S_IWUSR)
+
+
+def prepare_and_enter_chroot():
+    _mount_vkfs()
+    _enter_chroot()
+    _make_additional_dirs()
+
+
